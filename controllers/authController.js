94136acd71e77b51787aa5453c7cf2c893d7ef48
ALI <alirezaei.eng7@gmail.com>
2026-01-21 12:31:16 +0330

    complete auth user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import User from "../models/User.js";
-
+import { generateToken } from "../lib/utils/generateToken.js";
+import { jwtVerify } from "jose";
+import { JWT_SECRET } from "../lib/utils/constants.js";
+// create new user
 export const registerUser = async (req, res, next) => {
   try {
     // get data
@@ -20,15 +23,96 @@ export const registerUser = async (req, res, next) => {
 
     // create user in db
     const user = await User.create({ name, email, password });
-    console.log(user);
+
+    // Create tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = generateToken(payload, "2m");
+    const refreshToken = generateToken(payload, "30d");
+
+    // Set refresh token in HTTP-Only cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 30 * 60 * 60 * 24 * 1000, //30days
+    });
+
     res.status(201).json({
       message: "user created successfully",
+      accessToken,
       user: {
         id: user._id,
         name: user.name,
         email: user.email,
       },
     });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+// login user with email and password
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body || {};
+
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("email and password are required");
+    }
+    // Find user
+    const user = await User.findOne({ email });
+
+    // check if user not existing
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid Credentials");
+    }
+    // check if password matches
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      res.status(401);
+      throw new Error("Invalid Credentials");
+    }
+
+    // Create tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = generateToken(payload, "2m");
+    const refreshToken = generateToken(payload, "30d");
+
+    // Set refresh token in HTTP-Only cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 30 * 60 * 60 * 24 * 1000, //30days
+    });
+
+    res.status(201).json({
+      message: "user logged in successfully",
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+// Logout user and clear refresh token
+export const logoutUser = async (req, res, next) => {
+  try {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+    });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (err) {
     console.log(err);
     next(err);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/authController.js";
 const router = express.Router();
 
 // @route        POST api/auth/register
 // @description  Create New User
 // @access       Public
 router.post("/register", registerUser);
+
+// @route        POST api/auth/login
+// @description  Authenticate User
+// @access       Public
+router.post("/login", loginUser);
+
+// @route        POST api/auth/logout
+// @description  Logout user and clear refresh token
+// @access       Private
+router.post("/logout", logoutUser);
 
 export default router;
